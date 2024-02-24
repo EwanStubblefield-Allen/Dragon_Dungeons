210fdc92d978b8c5c2e4a83b913940040e46075b
EwanStubblefield-Allen <ewan2004@yahoo.com>
2024-02-24 13:23:51 -0700

    Changed port mapping

--- Dragon_Dungeons.client/src/services/ImagesService.js
+++ Dragon_Dungeons.client/src/services/ImagesService.js
@@ -17,21 +17,6 @@ class ImagesService {
     await api.post('api/images/deletion', { public_id: imageId })
   }
 
-  async sha256(message) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message)
-
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
-
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-
-    // convert bytes to hex string
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-    return hashHex
-  }
-
   async compress(file, compress) {
     await new Promise(resolve => {
       new Compressor(file, {
